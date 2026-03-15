5531b65fb095ea2c7e779c18b02581f8cea0ec66
tumpopoM <wijitrarattanason@gmail.com>
2026-03-15 17:09:11 +0700

    feat: add unit tests for todo provider

---
+++ test/todo_provider_test.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_flutter_app/providers/todo_provider.dart';
+import 'package:todo_flutter_app/models/todo_model.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences.setMockInitialValues({});
+  test('addTodo should add new todo', () {
+    final notifier = TodoNotifier();
+
+    notifier.addTodo("Test title", "Test description", DateTime.now(), null);
+
+    expect(notifier.state.length, 1);
+    expect(notifier.state.first.title, "Test title");
+  });
+
+  test('removeTodo should delete todo', () {
+    final notifier = TodoNotifier();
+
+    notifier.addTodo("Test", "Test desc", DateTime.now(), null);
+
+    final id = notifier.state.first.id;
+
+    notifier.removeTodo(id);
+
+    expect(notifier.state.isEmpty, true);
+  });
+
+  test('updateTodo should update todo fields', () {
+    final notifier = TodoNotifier();
+
+    notifier.addTodo("Old title", "Old desc", DateTime.now(), null);
+
+    final todo = notifier.state.first;
+
+    notifier.updateTodo(
+      todo.id,
+      "New title",
+      "New desc",
+      DateTime.now(),
+      null,
+      "IN_PROGRESS",
+    );
+
+    expect(notifier.state.first.title, "New title");
+  });
+
+  test('toggleTodo should change status', () {
+    final notifier = TodoNotifier();
+
+    notifier.addTodo("Test", "Test desc", DateTime.now(), null);
+
+    final id = notifier.state.first.id;
+
+    notifier.toggleTodo(id);
+
+    expect(notifier.state.first.status, "COMPLETED");
+  });
+
+  test('toggleTodo should change status', () {
+    final notifier = TodoNotifier();
+
+    notifier.addTodo("Test", "Test desc", DateTime.now(), null);
+
+    final id = notifier.state.first.id;
+
+    notifier.toggleTodo(id);
+
+    expect(notifier.state.first.status, "COMPLETED");
+  });
+}
